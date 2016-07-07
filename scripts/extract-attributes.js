f586295b4163e5a6679a5b55dc5e16c78f953149
Per Nilsson <per.nilsson@formidablelabs.com>
2016-07-07 16:05:00 -0700

    Ensure attributes use React camelcasing as props

--- scripts/extract-attributes.js
+++ scripts/extract-attributes.js
@@ -9,26 +9,45 @@ const htmlURL = 'https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes';
 const dataPath = './data/attributes.json';
 
 // From https://facebook.github.io/react/docs/tags-and-attributes.html#supported-attributes
-// less the `data` attribute
-const supportedAttributes = ['accept', 'acceptCharset', 'accessKey', 'action',
+// less the `data` attribute,
+// the special `className` and `htmlFor` props,
+// and `httpEquiv` + `acceptCharset` which are already correctly camelCased.
+const supportedAttributes = ['accept', 'accessKey', 'action',
 'allowFullScreen', 'allowTransparency', 'alt', 'async', 'autoComplete',
 'autoFocus', 'autoPlay', 'capture', 'cellPadding', 'cellSpacing', 'challenge',
-'charSet', 'checked', 'cite', 'classID', 'className', 'colSpan', 'cols',
-'content', 'contentEditable', 'contextMenu', 'controls', 'coords',
-'crossOrigin', 'dateTime', 'default', 'defer', 'dir', 'disabled',
-'download', 'draggable', 'encType', 'form', 'formAction', 'formEncType',
-'formMethod', 'formNoValidate', 'formTarget', 'frameBorder', 'headers',
-'height', 'hidden', 'high', 'href', 'hrefLang', 'htmlFor', 'httpEquiv', 'icon',
-'id', 'inputMode', 'integrity', 'is', 'keyParams', 'keyType', 'kind', 'label',
-'lang', 'list', 'loop', 'low', 'manifest', 'marginHeight', 'marginWidth', 'max',
-'maxLength', 'media', 'mediaGroup', 'method', 'min', 'minLength', 'multiple',
-'muted', 'name', 'noValidate', 'nonce', 'open', 'optimum', 'pattern',
-'placeholder', 'poster', 'preload', 'profile', 'radioGroup', 'readOnly', 'rel',
-'required', 'reversed', 'role', 'rowSpan', 'rows', 'sandbox', 'scope', 'scoped',
-'scrolling', 'seamless', 'selected', 'shape', 'size', 'sizes', 'span',
-'spellCheck', 'src', 'srcDoc', 'srcLang', 'srcSet', 'start', 'step', 'style',
-'summary', 'tabIndex', 'target', 'title', 'type', 'useMap', 'value', 'width',
-'wmode', 'wrap'];
+'charSet', 'checked', 'cite', 'classID', 'colSpan', 'cols', 'content',
+'contentEditable', 'contextMenu', 'controls', 'coords', 'crossOrigin',
+'dateTime', 'default', 'defer', 'dir', 'disabled', 'download', 'draggable',
+'encType', 'form', 'formAction', 'formEncType', 'formMethod', 'formNoValidate',
+'formTarget', 'frameBorder', 'headers', 'height', 'hidden', 'high', 'href',
+'hrefLang', 'icon', 'id', 'inputMode', 'integrity', 'is',
+'keyParams', 'keyType', 'kind', 'label', 'lang', 'list', 'loop', 'low',
+'manifest', 'marginHeight', 'marginWidth', 'max', 'maxLength', 'media',
+'mediaGroup', 'method', 'min', 'minLength', 'multiple', 'muted', 'name',
+'noValidate', 'nonce', 'open', 'optimum', 'pattern', 'placeholder', 'poster',
+'preload', 'profile', 'radioGroup', 'readOnly', 'rel', 'required', 'reversed',
+'role', 'rowSpan', 'rows', 'sandbox', 'scope', 'scoped', 'scrolling',
+'seamless', 'selected', 'shape', 'size', 'sizes', 'span', 'spellCheck', 'src',
+'srcDoc', 'srcLang', 'srcSet', 'start', 'step', 'style', 'summary', 'tabIndex',
+'target', 'title', 'type', 'useMap', 'value', 'width', 'wmode', 'wrap'];
+
+// Create a map of HTML attribute to React prop
+// e.g. {"datetime": "dateTime"}
+const attributeMap = supportedAttributes.reduce((map, reactAttribute) => {
+    const htmlAttribute = reactAttribute.toLowerCase();
+    map[htmlAttribute] = reactAttribute;
+
+    return map;
+},
+    // Start the map with two attributes that have special names in React, and
+    // two attributes that already have camelCasing in HTML.
+    {
+        'class': 'className',
+        'for': 'htmlFor',
+        'httpEquiv': 'httpEquiv',
+        'acceptCharset': 'acceptCharset'
+    }
+);
 
 /**
  * From the MDN attributes reference, extract a map of attributes with
@@ -41,36 +60,37 @@ function extractAttributes($) {
     $table.find('tbody tr').each((i, row) => {
         const $children = cheerio(row).find('td');
         const attribute = $children.eq(0).text();
+
         const elements = $children.eq(1).text()
             .replace(/[\<\>\s]/g, '')
             .split(',');
+
         const description = $children.eq(2).text()
             .replace(/\n/g, '')
             // Fix irregular whitespace characters
             .replace(' ', ' ')
             .trim();
 
-        // Trim attribute, and convert e.g. `accept-charset` to `acceptCharset`.
-        const attributeName = S(attribute)
+        const htmlAttribute = S(attribute)
             .trim()
+            // Convert e.g. `accept-charset` to `acceptCharset`
             .camelize()
-            .toString()
-            // Rename `class` to `className`
-            .replace(/^class$/, 'className')
-            // Rename `for` to `htmlFor`
-            .replace(/^for$/, 'htmlFor');
+            .toString();
 
         // Skip `data-*` attributes
-        if (attributeName.indexOf('data') === 0) {
+        if (htmlAttribute.indexOf('data') === 0) {
             return true;
         }
 
         // Ensure attribute is supported by React
-        if (supportedAttributes.indexOf(attributeName) === -1) {
+        if (!attributeMap[htmlAttribute]) {
             return true;
         }
 
-        attributes[attributeName] = {
+        // maxlength -> maxLength; class -> className
+        const reactAttribute = attributeMap[htmlAttribute];
+
+        attributes[reactAttribute] = {
             elements,
             description
         };
