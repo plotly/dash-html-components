9a0c3302366e5f8a05a0cf5a4a246b4badf77ac1
Per Nilsson <per.nilsson@formidablelabs.com>
2016-07-31 13:39:39 -0700

    Support dynamic loading of component JS bundle

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='dash_html_components',
     version='0.2.3',
     author='plotly',
-    packages=['dash_html_components'],
-    package_data={'dash_html_components': ['metadata.json']},
+    packages=['dash_html_components', 'lib'],
+    package_data={'lib': ['../lib/*']},
     license='MIT',
     description='Dash UI component suite',
     install_requires=['flask', 'plotly', 'flask-cors', 'dash.ly']
