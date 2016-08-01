092141e027def86d99e800ea250a84345552b80f
Per Nilsson <per.nilsson@formidablelabs.com>
2016-07-31 17:36:52 -0700

    Use data_files to copy files to site-packages
    The reason we're playing whack-a-mole with these datafiles
    is that MANIFEST is ignored with doing `python setup.py install`,
    so we need ot use data_files or package_data. package_data for a
    `lib` package ended up creating a sibling directory `lib/` in
    site-packages, instead of nesting under `dash_html_components`.
    
    The hope is that using data_files will actually copy files in under
    dash_html_components consistently for local, and remote installs,
    so we can call it a day.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='dash_html_components',
-    version='0.2.3',
+    version='0.3.0',
     author='plotly',
-    packages=['dash_html_components', 'lib'],
-    package_data={'lib': ['../lib/*']},
+    packages=['dash_html_components'],
+    data_files=[('js', ['lib/bundle.js', 'lib/metadata.json'])],
     license='MIT',
     description='Dash UI component suite',
     install_requires=['flask', 'plotly', 'flask-cors', 'dash.ly']
