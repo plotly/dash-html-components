da9ea98a08ccb9812bffdd72a28b4ef75b1006c6
Per Nilsson <per.nilsson@formidablelabs.com>
2016-07-31 18:01:02 -0700

    PyPi v0.3.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dash_html_components',
-    version='0.3.2',
+    version='0.3.3',
     author='plotly',
     packages=['dash_html_components', 'dash_html_components.lib'],
     package_data={'dash_html_components.lib': ['../lib/*']},
