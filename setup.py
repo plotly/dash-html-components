3d76e8d2f2767ff4bf520f3018190339e651e29c
Per Nilsson <per.nilsson@formidablelabs.com>
2016-07-31 18:27:55 -0700

    Fourth time's the charm - use MANIFEST.in
    If we specify `include_package_data=True` in setup.py,
    the MANIFEST is honored when running `python setup.py install`!

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(
     name='dash_html_components',
     version='0.3.3',
-    author='plotly',
-    packages=['dash_html_components', 'dash_html_components.lib'],
-    package_data={'dash_html_components.lib': ['../lib/*']},
+    author='Chris Parmer',
+    author_email='chris@plot.ly',
+    packages=['dash_html_components'],
+    include_package_data=True,
     license='MIT',
     description='Dash UI component suite',
     install_requires=['flask', 'plotly', 'flask-cors', 'dash.ly']
