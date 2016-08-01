07f280b3ddf7287485a9566a27d3ebb32efdc939
Per Nilsson <per.nilsson@formidablelabs.com>
2016-07-31 17:48:08 -0700

    Try specifying data_files inside package
    The previous approach resulted in this error when running
    pip install `dash-html-components`:
    ```
    error: can't copy 'lib/bundle.js': doesn't exist or not a regular file
    ```
    
    It appears when installing from a remote location, the files
    are expected to exist at install time; however they are not included
    when publishing to pypi.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.3.1',
     author='plotly',
     packages=['dash_html_components'],
-    data_files=[('js', ['lib/bundle.js', 'lib/metadata.json'])],
+    data_files=[('dash_html_components', ['dash_html_components/bundle.js', 'dash_html_components/metadata.json'])],
     license='MIT',
     description='Dash UI component suite',
     install_requires=['flask', 'plotly', 'flask-cors', 'dash.ly']
