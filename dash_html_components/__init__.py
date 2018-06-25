c5069e8ee5145f543bffbd50d715a64dfe7f0490
Ryan Marren <rymarr@tuta.io>
2018-06-24 22:16:37 -0400

    Update __init__.py to use IDE compatible imports

--- dash_html_components/__init__.py
+++ dash_html_components/__init__.py
@@ -2,11 +2,10 @@ from __future__ import print_function as _
 
 import os as _os
 import sys as _sys
-import glob as _glob
-from importlib import import_module as _import
-
 import dash as _dash
 
+from ._imports_ import *
+from ._imports_ import __all__
 from .version import __version__
 
 
@@ -15,22 +14,9 @@ if not hasattr(_dash, 'development'):
           "named \n'dash.py' in your current directory.", file=_sys.stderr)
     _sys.exit(1)
 
+
 _current_path = _os.path.dirname(_os.path.abspath(__file__))
 
-_component_files = map(
-    lambda x: _os.path.splitext(_os.path.basename(x))[0],
-    filter(
-        lambda x: _os.path.basename(x) not in ['__init__.py', 'version.py'],
-        _glob.glob(_os.path.join(_current_path, '*.py'))
-    )
-)
-
-_components = [
-    getattr(
-        _import(".{:s}".format(c), package='dash_html_components'),
-        c
-    ) for c in _component_files
-]
 
 _this_module = _sys.modules[__name__]
 
@@ -49,7 +35,6 @@ _js_dist = [
 _css_dist = []
 
 
-for _component in _components:
-    setattr(_this_module, _component.__name__, _component)
-    setattr(_component, '_js_dist', _js_dist)
-    setattr(_component, '_css_dist', _css_dist)
+for _component in __all__:
+    setattr(locals()[_component], '_js_dist', _js_dist)
+    setattr(locals()[_component], '_css_dist', _css_dist)
