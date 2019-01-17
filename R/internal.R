ecd8e538e225deeaed582c7193bd4163589eee8f
Ryan Patrick Kyle <ryan@plot.ly>
2019-01-17 18:59:21 -0500

    fixing deps issue

--- R/internal.R
+++ R/internal.R
@@ -1,7 +1,7 @@
 .dashHtmlComponents_js_metadata <- function() {
 deps_metadata <- list(`dash_html_components` = structure(list(name = "dash_html_components",
 version = "0.13.2", src = list(href = NULL,
-file = "lib/"), meta = NULL,
+file = "deps/"), meta = NULL,
 script = "dash_html_components.min.js",
 stylesheet = NULL, head = NULL, attachment = NULL, package = "dashHtmlComponents",
 all_files = FALSE), class = "html_dependency"))
