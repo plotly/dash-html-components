55c3884f3f02fcb73d733b519683a4c08d8c8906
Ryan Patrick Kyle <rpkyle@users.noreply.github.com>
2019-12-19 12:09:15 -0500

    Update dashHtmlComponents to 1.0.2 with support for async loading in R (#146)

--- R/internal.R
+++ R/internal.R
@@ -10,7 +10,7 @@ version = "1.0.2", src = list(href = NULL,
 file = "deps"), meta = NULL,
 script = 'dash_html_components.min.js.map',
 stylesheet = NULL, head = NULL, attachment = NULL, package = "dashHtmlComponents",
-all_files = FALSE), class = "html_dependency"))
+all_files = FALSE, dynamic = TRUE), class = "html_dependency"))
 return(deps_metadata)
 }
 
