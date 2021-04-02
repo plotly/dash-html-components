47f7f046d65a6b86d72d00009341f4dd1dd2be12
Marc-André Rivet <marc-andre.rivet@plotly.com>
2021-04-02 14:43:46 -0400

    remove css

--- webpack.config.js
+++ webpack.config.js
@@ -60,37 +60,7 @@ module.exports = (env, argv) => {
                     use: {
                         loader: 'babel-loader',
                     },
-                },
-                {
-                    test: /\.css$/,
-                    use: [
-                        {
-                            loader: 'style-loader',
-                            options: {
-                                insert: function insertAtTop(element) {
-                                    var parent = document.querySelector('head');
-                                    // eslint-disable-next-line no-underscore-dangle
-                                    var lastInsertedElement =
-                                        window._lastElementInsertedByStyleLoader;
-
-                                    if (!lastInsertedElement) {
-                                        parent.insertBefore(element, parent.firstChild);
-                                    } else if (lastInsertedElement.nextSibling) {
-                                        parent.insertBefore(element, lastInsertedElement.nextSibling);
-                                    } else {
-                                        parent.appendChild(element);
-                                    }
-
-                                    // eslint-disable-next-line no-underscore-dangle
-                                    window._lastElementInsertedByStyleLoader = element;
-                                }
-                            }
-                        },
-                        {
-                            loader: 'css-loader',
-                        },
-                    ],
-                },
+                }
             ],
         },
         devtool
