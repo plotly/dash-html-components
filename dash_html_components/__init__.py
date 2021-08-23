f686d6062916cf4c844126daac6c1e48a97c6e17
Jon Mease <jon.mease.dev@gmail.com>
2021-08-23 18:10:16 -0400

    Create Dash 2 stub package

---
+++ dash_html_components/__init__.py
@@ -0,0 +1,9 @@
+from dash.html import *  # noqa: F401, F403, E402
+import warnings
+
+warnings.warn(
+    """
+The dash_html_components package is deprecated. Please replace
+`import dash_html_components as html` with `from dash import html`""",
+    stacklevel=2,
+)
