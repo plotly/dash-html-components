62a22bb264d1072806ba2d682de4e4b556af85c1
Alexandr Romanenko <waralex@gmail.com>
2021-10-05 10:52:43 +0300

    Empty Julia Package

---
+++ src/DashHtmlComponents.jl
@@ -0,0 +1,5 @@
+module DashHtmlComponents
+    function __init__()
+        @warn "`using DashHtmlComponents` is deprecated, all DashHtmlComponents functions have been moved to Dash package. Use `using Dash` instead"
+    end
+end
